778c2422c30caf71863f92866a3e3915f74963fa
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2016-08-17 23:55:37 +0700

    string binary

--- strings.go
+++ strings.go
@@ -5,10 +5,20 @@ import (
 	"strings"
 )
 
+func binary() {
+	firstName := []byte("Eko Kurniawan")
+	lastName := string([]byte("Khannedy"))
+
+	fmt.Println(string(firstName))
+	fmt.Println(lastName)
+}
+
 func main() {
 	name := "Eko Kurniawan Khannedy"
 	fmt.Println(strings.Contains(strings.ToLower(name), "eko"))
 
 	fmt.Println(len(name))
 	fmt.Println(strings.Count(name, "a"))
+
+	binary()
 }
