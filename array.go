7af282f0378ac1693ce77a4ebf96ff804a044a7f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2016-08-17 11:04:32 +0700

    add array.go

---
+++ array.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+	Array()
+	ShortArray()
+}
+
+func ShortArray() {
+	array := [5]int{
+		1, 2, 3, 4, 5,
+	}
+
+	for _, value := range array {
+		fmt.Println(value)
+	}
+}
+
+func Array() {
+	var array [10]int
+
+	array[0] = rand.Intn(10)
+	array[1] = rand.Intn(10)
+	array[2] = rand.Intn(10)
+	array[3] = rand.Intn(10)
+	array[4] = rand.Intn(10)
+	array[5] = rand.Intn(10)
+	array[6] = rand.Intn(10)
+	array[7] = rand.Intn(10)
+	array[8] = rand.Intn(10)
+	array[9] = rand.Intn(10)
+
+	fmt.Println(array)
+
+	var total int = 0
+
+	for i := 0; i < len(array); i++ {
+		total += array[i]
+	}
+
+	fmt.Println(total)
+
+	total = 0
+	for _, value := range array {
+		total += value
+	}
+
+	fmt.Println(total)
+
+}
