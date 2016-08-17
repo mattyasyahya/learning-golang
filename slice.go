8bafd25d12d614f71ff333a6246406f250360750
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2016-08-17 12:21:32 +0700

    update slice.go

--- slice.go
+++ slice.go
@@ -5,6 +5,15 @@ import "fmt"
 func main() {
 	Append()
 	Copy()
+	Slice()
+}
+
+func Slice() {
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	fmt.Println(data[2:7])
+	fmt.Println(data[:7])
+	fmt.Println(data[2:])
+	fmt.Println(data[:])
 }
 
 func Copy() {
