291cf7ee3ab239f9ad320bc30ecb7f9abd5981d8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2016-08-18 19:32:39 +0700

    channel

--- channel.go
+++ channel.go
@@ -5,16 +5,24 @@ import (
 	"time"
 )
 
-func pinger(c chan string) {
+func ponger(c chan<- string) {
 	for i := 0; i < 10; i++ {
+		fmt.Println("send pong")
+		c <- "pong"
+	}
+}
+
+func pinger(c chan<- string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("send ping")
 		c <- "ping"
 	}
 }
 
-func printer(c chan string) {
+func printer(c <-chan string) {
 	for {
 		msg := <-c
-		fmt.Println(msg)
+		fmt.Println("receive", msg)
 		time.Sleep(time.Second * 1)
 	}
 }
@@ -22,6 +30,7 @@ func printer(c chan string) {
 func main() {
 	var c = make(chan string)
 	go pinger(c)
+	go ponger(c)
 	go printer(c)
 
 	var input string
